789f800eaafa84945caf8c8289ee49f1a43afc08
SamuelCba <152031902+SamuelCba@users.noreply.github.com>
2024-11-03 21:24:52 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,27 +4,11 @@ bars.onclick = function() {
     navBar.classList.toggle('active');
 }
 
-const audio = document.getElementById('audio');
-const playPauseBtn = document.getElementById('playPauseBtn');
-const progress = document.getElementById('progress');
 
-// Alterna entre reproducir y pausar
-playPauseBtn.addEventListener('click', () => {
-    if (audio.paused) {
-        audio.play();
-        playPauseBtn.textContent = '⏸️';
-    } else {
-        audio.pause();
-        playPauseBtn.textContent = '▶️';
-    }
-});
+if (navBar.classList.contains("active")) {
+    body.style.overflow = "hidden";
+} else {
+    body.style.overflow =  "auto";
+ }
 
-// Actualiza la barra de progreso
-audio.addEventListener('timeupdate', () => {
-    progress.value = (audio.currentTime / audio.duration) * 100;
-});
-
-// Permite avanzar el audio al mover la barra de progreso
-progress.addEventListener('input', () => {
-    audio.currentTime = (progress.value / 100) * audio.duration;
-});
+ 
